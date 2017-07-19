69f533b24da0350ec91b5af9b924cd7953704ddb
Tim Haines <tmhaines@gmail.com>
2017-07-19 11:04:23 -0700

    Handle installation_id not found, and redirect to right page.

--- app/routes/github-app-setup.js
+++ app/routes/github-app-setup.js
@@ -9,11 +9,45 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
   },
 
   model(params) {
+    // Find the organization the user added the GitHub integration to,
+    // and then redirect them back to the org setup flow (or settings if they already have projects)
+
     this.get('currentUser.organizations').then((orgs) => {
+      // Attempt to get the organization that matches the installation_id
+      // This may fail if we haven't received the webhook yet, or a fake param is used
       let organization = orgs.find(
         (org) => org.get('githubIntegration.githubInstallationId') == params.installation_id
       );
-      this.replaceWith('organization.index', organization.get('slug'));
+
+      // If no org, next try and get the user's most recent org (in case they have multiple)
+      if (!organization && window.localStorage) {
+        let lastOrganizationSlug = window.localStorage.getItem('lastOrganizationSlug');
+        if (lastOrganizationSlug) {
+          organization = orgs.find(
+            (org) => org.get('slug') == lastOrganizationSlug
+          );
+        }
+      }
+
+      // If no organization, use the first one in the user's list.  Weird edge case that we don't expect.
+      if (!organization) {
+        organization = orgs.get('firstObject');
+      }
+
+      // If the user has no organization, redirect them to the home page
+      if (!organization) {
+        this.replaceWith('index');
+      }
+
+      // If org has projects, redirect to the settings page, else redirect to add a project page.
+      organization.get('projects').then((projects) => {
+        if (projects.get('length') > 0) {
+          this.replaceWith('organizations.organization.settings', organization.get('slug'));
+        } else {
+          this.replaceWith('organization.index', organization.get('slug'));
+        }
+      });
+
     });
   },
 });
